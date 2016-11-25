524965e14fa9b3285330b66904f9c03e57698b4e
Rainer Ross <rainer_ross@web.de>
2016-11-25 12:36:54 +0100

    Update chatclient.js

--- chatclient.js
+++ chatclient.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-var socket = io.connect();
-socket.on('chat', function(data) {					// Add chat data to content
-$('#content').append(
+	var socket = io.connect();
+	socket.on('chat', function(data) {					// Add chat data to content
+	$('#content').append(
 				$('<li></li>').append(
 						$('<span>').text('[' + getTime(data) + '] '),
 						$('<b>').text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
@@ -36,3 +36,4 @@ String.prototype.lpad = function(padString, length) {	// add leading padding cha
 	}
 	return string;
 };
+	
